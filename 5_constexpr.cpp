2fa0197ecdf95ff1083b050f4be7ecd3a8204063
Chansik Yun <chansigi@gmail.com>
2020-09-21 13:19:53 +0900

    .

---
+++ 5_constexpr.cpp
@@ -0,0 +1,36 @@
+// 5_constexpr - C++11
+//  : 컴파일 타임 상수를 지정할 때 사용한다.
+//    컴파일 타임 상수를 만족하지 못할 때는 컴파일 오류가 발생한다.
+
+// C - const(상수)
+//  컴파일 타임 상수
+//  런타임 상수       - 메모리 공간이 존재한다.
+// #include <stdio.h>
+#include <iostream>
+using namespace std;
+
+#if 0
+
+int main() {
+  const int a = 10;
+  constexpr int b = 10;
+
+  int n;
+  cin >> n;
+
+  const int c = n;
+  // constexpr int d = n;  // error!
+}
+#endif
+
+#if 1
+int main() {
+  constexpr int a = 42;
+  int* p = (int *)&a;
+
+  *p = 100;
+                       //  C     C++
+  printf("%d\n", *p);  // 100    100
+  printf("%d\n", a);   // 100    42
+}
+#endif
