7427cded367f694963446be50ae83577169d7b53
Chansik Yun <chansigi@gmail.com>
2020-09-21 14:44:27 +0900

    .

--- foo.cpp
+++ foo.cpp
@@ -3,10 +3,24 @@
 // 구현부
 #include "foo.h"
 
-int add(int a, int b) {
+extern int add(int a, int b) {
   return a + b;
 }
 
-int sub(int a, int b) {
+extern int sub(int a, int b) {
   return a - b;
 }
+
+// 전역 변수 / 전역 함수 - 연결(Linkage)
+//  1. extern(기본)
+//  : 다른 파일에서 해당 함수나 변수를 참조할 수 있는가?
+//   External Linkage
+//  2. static
+//  : 다른 파일에서 해당 함수나 변수를 참조할 수 없다.
+//    오직 같은 파일에서만 참조가 가능하다.
+//   Internal Linkage
+
+// 함수 내 변수(storage class)
+//  1. auto - 스택에 저장되는 메모리
+//  2. static - 정적 메모리
+//   : 함수가 처음 호출되는 시점에 생성되고, 더 이상 사용되지 않을 때 파괴된다.
--- foo.h
+++ foo.h
@@ -4,8 +4,8 @@
 #define FOO_H_
 
 // 선언부
-int add(int a = 10, int b = 20);
-int sub(int, int);
+extern int add(int a = 10, int b = 20);
+extern int sub(int, int);
 
 // 1. 헤더 파일안에는 internal linkage를 갖는 심볼만 작성하는 것이다.
 // 2. 중복 포함을 방지해야 합니다.
@@ -16,3 +16,5 @@ struct node {
 };
 
 #endif
+
+
