5b900f968c59d92c8e1a8e74224fb90e90459b4d
Chansik Yun <chansigi@gmail.com>
2020-09-21 16:01:14 +0900

    .

--- 7_function2.cpp
+++ 7_function2.cpp
@@ -4,7 +4,7 @@
 // 1. 디폴트 파라미터
 // 2. 함수 오버로딩(Function Overloading)
 //  : 파라미터의 타입과 개수가 다르면, 동일한 이름을 가지는 함수를 여러개 만들 수 있습니다.
-#if 1
+#if 0
 #include <iostream>
 using namespace std;
 
@@ -15,7 +15,7 @@ int square(int a) {
 double square(double d) {
   return d * d;
 }
-#endif
+
 
 // 매크로 함수를 이용해서, 비슷한 동작을 수행하는 것이 가능합니다.
 int main() {
@@ -24,6 +24,7 @@ int main() {
   cout << square(++a) << endl;
   cout << square(3.14) << endl;
 }
+#endif
 
 #if 0
 #include <stdio.h>
@@ -52,3 +53,38 @@ int main() {
 
 }
 #endif
+
+#include <iostream>
+using namespace std;
+
+// OK
+void foo(int a) {}
+void foo(double d) {}
+void foo(int a, int b) {}
+
+// 인자의 개수가 다르지만, 디폴트 파라미터가 있습니다.
+// void goo(int a) {}
+// void goo(int a, int b = 30) {}
+
+// 어떤 함수와 연결될지에 대한 부분에, 반환 타입은 중요하지 않습니다.
+// int hoo(int a) {}
+// void hoo(int a) {}
+
+int main() {
+  // goo(42);
+}
+
+// C언어의 함수는 컴파일 시 이름이 유지된다.
+// C++은 함수의 인자 정보를 토대로, 컴파일 시 함수의 이름이 변경된다. => 네임 맹글링(mangling) - 짓이기다
+
+//                                          extern "C"                                            
+//  foo(int)           -> _Z3fooi
+//  foo(int, int)      -> _Z3fooii       ->   foo
+//  foo(int, int, int) -> _Z3fooiii
+
+
+// C언어에서 C++의 함수를 호출하는 것이 가능할까요?
+// C++에서 C언어의 함수를 호출하는 것이 가능할까요?
+//  => extern "C"
+//    : C 함수의 심볼을 통해 함수를 제공한다.
+//    - 함수의 네임 맹글링을 수행하지 않기 때문에, 오버로딩이 불가능해진다.
