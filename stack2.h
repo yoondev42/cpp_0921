44dd1861cb558c4822eeb731fe24ff701c132560
Chansik Yun <chansigi@gmail.com>
2020-09-22 16:10:57 +0900

    .

--- stack2.h
+++ stack2.h
@@ -1,11 +1,17 @@
 // stack.h
 
-// 1. include guard
-//  => 헤더 파일의 중복 포함을 방지한다.
 #ifndef STACK_H
 #define STACK_H
 
-// 코딩 관례
+#include <iostream>
+
+// 아래의 스택은 int 타입에 대해서만 가능합니다.
+// template을 도입하면, int 뿐 아니라 
+// 모든 타입에 대한 stack을 제공하는 것이 가능합니다.
+
+// 핵심: 일반적인 클래스는 헤더와 소스로 분리하는 것이 가능합니다.
+// => 템플릿 클래스는 오직 헤더를 통해서만 제공해야 합니다.
+
 class Stack
 {
 public:
@@ -20,5 +26,34 @@ private:
   int *buff;
   int top;
 };
+
+Stack::Stack()
+{
+  buff = new int[10];
+  top = 0;
+}
+
+Stack::Stack(int size)
+{
+  buff = new int[size];
+  top = 0;
+}
+
+Stack::~Stack()
+{
+  std::cout << "~Stack()" << std::endl;
+  delete[] buff;
+}
+
+void Stack::push(int v)
+{
+  buff[top++] = v;
+}
+
+int Stack::pop()
+{
+  return buff[--top];
+}
+
 
 #endif
