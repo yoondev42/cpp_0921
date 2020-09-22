c94d309bc9c425e6302c0481cb5527f949441ac3
Chansik Yun <chansigi@gmail.com>
2020-09-22 11:51:15 +0900

    .

--- 10_casting.cpp
+++ 10_casting.cpp
@@ -10,15 +10,21 @@ using namespace std;
 //     캐스팅이 '버그의 원인'이 되는 경우가 많습니다.
 //  3. C++에서는 C 캐스팅의 문제점을 해결하기 위해서, 각 용도에 따른 4가지 종류의 캐스팅을 제공합니다.
 //    1. static_cast => 일반적인 캐스팅, 연관성이 없는 타입간의 캐스팅은 허용되지 않습니다.
-//    2. dynamic_cast
-//    3. const_cast
-//    4. reinterpret_cast
+//                      void*를 다른 타입으로 캐스팅 하는 것은 허용한다.
+//    2. dynamic_cast => 실행시간에 타입의 정보를 확인하고, 캐스팅합니다.(RTTI - Run Time Type Information)
+//    3. const_cast   => 상수성 제거를 위해 사용합니다.
+//    4. reinterpret_cast => 메모리를 재해석할 때 사용합니다. C의 캐스팅과 유사합니다.
 
 int main() {
   int n = 10;
   // double* p = (double *)&n; // OK!
 
-  double* p = static_cast<double*>(&n);
+  // error: static_cast from 'int *' to 'double *' is not allowed
+  // double* p = static_cast<double*>(&n);
+  double* p = reinterpret_cast<double*>(&n);
+
+  // int *p = (int *)malloc(sizeof(int) * 10);
+  // int* p = static_cast<int*>(malloc(sizeof(int) * 10));
 
 }
 
