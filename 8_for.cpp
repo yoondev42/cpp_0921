3c4ca518c2816a14dea53719e8ce1ceda4b3bdfd
Chansik Yun <chansigi@gmail.com>
2020-09-22 09:35:56 +0900

    .

--- 8_for.cpp
+++ 8_for.cpp
@@ -2,6 +2,28 @@
 #include <iostream>
 using namespace std;
 
+int foo() {
+  return 42;
+}
+
+int main() {
+  int ret = foo();
+  if (ret == 0) {
+    printf("ret가 0일때의 동작\n");
+  }
+  printf("ret: %d\n", ret);
+
+  // g++ 8_for.cpp -std=c++17
+  // C++17 => 초기화 구문이 추가된 제어문(if-statement with initializer)
+  if (int ret2 = foo(); ret2 == 0) {
+    printf("ret2가 0일때의 동작\n");
+  }
+  // printf("ret: %d\n", ret2);  - ret2는 if 블록을 벗어나면 파괴된다.
+}
+
+
+
+#if 0
 int main() {
   int x[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
@@ -15,3 +37,4 @@ int main() {
     cout << n << endl;
   }
 }
+#endif
