c7e8ad44c60c25bb8295eb4a1169c7b1aa610c0f
Chansik Yun <chansigi@gmail.com>
2020-09-22 16:20:07 +0900

    .

--- stack2.h
+++ stack2.h
@@ -6,12 +6,11 @@
 #include <iostream>
 
 // 아래의 스택은 int 타입에 대해서만 가능합니다.
-// template을 도입하면, int 뿐 아니라 
+// template을 도입하면, int 뿐 아니라
 // 모든 타입에 대한 stack을 제공하는 것이 가능합니다.
 
 // 핵심: 일반적인 클래스는 헤더와 소스로 분리하는 것이 가능합니다.
 // => 템플릿 클래스는 오직 헤더를 통해서만 제공해야 합니다.
-
 
 // Stack: class template
 // => Stack<int>    : class type
@@ -25,8 +24,8 @@ public:
   Stack(int size);
   ~Stack();
 
-  void push(int v);
-  int pop();
+  void push(const T &v);
+  T pop();
 
 private:
   T *buff;
@@ -55,16 +54,15 @@ Stack<T>::~Stack()
 }
 
 template <typename T>
-void Stack<T>::push(int v)
+void Stack<T>::push(const T &v)
 {
   buff[top++] = v;
 }
 
 template <typename T>
-int Stack<T>::pop()
+T Stack<T>::pop()
 {
   return buff[--top];
 }
-
 
 #endif
