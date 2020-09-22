d2ec06558ccc4effd03816294e5c2a3106995ca0
Chansik Yun <chansigi@gmail.com>
2020-09-22 14:50:42 +0900

    .

--- 14_stack.cpp
+++ 14_stack.cpp
@@ -4,7 +4,63 @@ using namespace std;
 //  => Last In First out(후입선출) 방식의 자료 구조 입니다.
 
 
-// Version 3. Stack 타입
+// Version 4. Stack 타입 - Stack의 상태를 push / pop이 통해서가 아니라, 직접 변경하면 문제가 발생합니다.
+//                        "정보 은닉" => 접근 지정자(private / public)
+
+// struct
+//  : 외부에서 접근이 기본적으로 허용됩니다. - public
+// class
+//  : 외부에서 접근이 기본적으로 허용되지 않습니다. - private
+//  => 객체를 생성하는 '틀'이라는 개념
+
+class Stack {
+// private
+// 외부에서 접근이 불가능합니다.
+//  - Stack의 멤버 함수를 통해서만 값의 변경이 가능합니다.
+private:  
+  // 멤버 변수
+  int buff[10];
+  int top;
+
+// 외부에서 접근이 가능합니다.
+public:
+  void init() {
+    top = 0;
+  }
+
+  // 구조체 내부의 필드값을 변경하는 함수를 구조체 내부에 둘 수 있습니다.
+  // => push / pop을 호출한 객체의 필드에 암묵적으로 접근하는 것이 가능합니다.
+  // 멤버 함수(메소드 - Method)
+  void push(int v) {
+    buff[top++] = v;
+  }
+
+  int pop() {
+    return buff[--top];
+  }
+};
+
+int main() {
+  Stack s1, s2;
+  // 사용하기 전에 초기화가 필요합니다.
+  // s1.top = 0;
+  // s2.top = 0;
+  s1.init();
+  s2.init();
+
+  s1.push(10);
+  s1.push(20);
+  s1.push(30);
+
+  // s1.top = 10;  // !?!?!?!?!?!?!
+  
+  cout << s1.pop() << endl;
+  cout << s1.pop() << endl;
+  cout << s1.pop() << endl;
+}
+
+#if 0
+// Version 3. Stack 타입 - 멤버 함수 도입
 struct Stack {
   // 멤버 변수
   int buff[10];
@@ -31,12 +87,14 @@ int main() {
   s1.push(10);
   s1.push(20);
   s1.push(30);
+
+  s1.top = 10;  // !?!?!?!?!?!?!
   
   cout << s1.pop() << endl;
   cout << s1.pop() << endl;
   cout << s1.pop() << endl;
 }
-
+#endif
 
 #if 0
 // Version 2. Stack 타입
