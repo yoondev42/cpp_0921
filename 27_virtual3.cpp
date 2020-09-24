8359db876cc9291019d960a173edba7c5d30ba7b
Chansik Yun <chansigi@gmail.com>
2020-09-24 14:40:49 +0900

    .

--- 27_virtual3.cpp
+++ 27_virtual3.cpp
@@ -1,4 +1,104 @@
 
+#include <iostream>
+#include <string>
+#include <set>            // Tree
+#include <unordered_set>  // HashTable
+using namespace std;
+
+// set - 집합 자료 구조
+//     => 교집합, 합집합, 여집합
+//     => 데이터의 중복을 허용하지 않습니다.
+
+int main() {
+  std::unordered_set<string> s;
+
+  s.insert("Bob");
+  s.insert("Tom");
+  s.insert("Alice");
+  s.insert("Bob");
+  s.insert("Tom");
+  s.insert("Alice");
+  s.insert("Bob");
+  s.insert("Tom");
+  s.insert("Alice");
+
+  cout << s.size() << endl;
+  for (auto e : s) {
+    cout << e << endl;
+  }
+}
+
+
+
+// 검색을 위한 컨테이너
+// map / unordered_map
+
+// map: key-value의 데이터 쌍의 집합
+//  "key - value를 찾아내기 위한 값"
+
+// Tree / Hash
+// 검색: O(logN) : O(1)
+
+// HashTable은 키의 순서가 보장되지 않는다.
+// 정렬된 데이터가 필요한 경우, 별도 정렬이 필요하다. -> O(NlogN)
+
+// Tree는 정렬된 데이터를 한번의 순회로 얻어낼 수 있다.
+//  -> O(N)
+#if 0
+#include <iostream>
+#include <map>
+#include <unordered_map>  // C++11
+#include <string>
+using namespace std;
+
+
+int main() {
+  unordered_map<string, int> m;
+
+  m["Tom"] = 42;
+  // "Tom" -> hash(k) -> bucket index -> value를 저장
+
+  m["Bob"] = 30;
+  m["Alice"] = 100;
+  m["Sam"] = 1000;
+
+  // Map -> std::pair 의 집합
+  //     k -> e.first
+  //     v -> e.second
+  for (std::pair<string, int> e : m) {
+    cout << e.first << " -> " << e.second << endl;
+  }
+}
+#endif
+
+#if 0
+int main() {
+  unordered_map<string, int> m;
+
+  m["Tom"] = 42;
+  // "Tom" -> hash(k) -> bucket index -> value를 저장
+
+  m["Bob"] = 30;
+  m["Alice"] = 100;
+
+  // O(logN) - map
+  // O(1)    - unordered_map
+  cout << m["Tom"] << endl;
+  cout << m["Bob"] << endl;
+  cout << m["Alice"] << endl;
+
+  // 없는 키를 검색할 경우, NULL(nullptr)이 나옵니다.
+  cout << m["Yoon"] << endl;
+}
+#endif
+
+
+
+
+
+
+
+#if 0
 // STL이 제공하는 기본 컨테이너의 간단한 사용방법.
 #include <iostream>
 #include <vector>
@@ -71,4 +171,5 @@ int main() {
     std::cout << e << std::endl;
   }
 }
+#endif
 #endif
